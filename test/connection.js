53eb68f920a2b6da4466c9b47ae4d68d67cdebf9
Levon Kirakosyan <noosx.e@gmail.com>
2014-06-24 11:34:53 +0400

    serial, parallel query execution

--- test/connection.js
+++ test/connection.js
@@ -59,8 +59,64 @@ describe('Connection', function() {
 					charset: 'UTF8_UNICODE_CI',
 					string: 'mysql://root:@localhost:3306/test?&charset=UTF8_UNICODE_CI',
 					debug: false
-				}).query('select 1 + $1 as result', { 1: 1 }))
+				}).query('select 1 + ? as result', [1]))
 					.to.eventually.be.deep.equal([{ result: 2 }]);
+
+			});
+
+		});
+
+		describe('#querySerial', function() {
+
+			it('should execute given queries serially in a single transaction', function() {
+
+				return expect(Connection.instance({
+					host: 'localhost',
+					port: '3306',
+					user: 'root',
+					password: '',
+					database: 'test',
+					charset: 'UTF8_UNICODE_CI',
+					string: 'mysql://root:@localhost:3306/test?&charset=UTF8_UNICODE_CI',
+					debug: false
+				}).querySerial([
+					{ text: 'select 1 + ? as result', values: [1] },
+					{ text: 'select 1 + ? as result', values: [2] },
+					{ text: 'select 1 + ? as result', values: [3] }
+				]))
+					.to.eventually.be.deep.equal([
+						[{ result: 2 }],
+						[{ result: 3 }],
+						[{ result: 4 }]
+					]);
+
+			});
+
+		});
+
+		describe('#queryParallel', function() {
+
+			it('should execute given queries in parallel', function() {
+
+				return expect(Connection.instance({
+					host: 'localhost',
+					port: '3306',
+					user: 'root',
+					password: '',
+					database: 'test',
+					charset: 'UTF8_UNICODE_CI',
+					string: 'mysql://root:@localhost:3306/test?&charset=UTF8_UNICODE_CI',
+					debug: false
+				}).queryParallel([
+					{ text: 'select 1 + ? as result', values: [1] },
+					{ text: 'select 1 + ? as result', values: [2] },
+					{ text: 'select 1 + ? as result', values: [3] }
+				]))
+					.to.eventually.be.deep.equal([
+						[{ result: 2 }],
+						[{ result: 3 }],
+						[{ result: 4 }]
+					]);
 
 			});
 
